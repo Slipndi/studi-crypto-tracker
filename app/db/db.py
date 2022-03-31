b98997e1b617a034982d290166026cd0cb15ccb8
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-03-31 12:30:01 +0200

    Mise en place BDD

--- app/db/db.py
+++ app/db/db.py
@@ -4,40 +4,11 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
-# Récupération de la base de donnée
-def get_db():
-    if 'db' not in g : 
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-    return g.db
+def get_db_connection() : 
+    connection = sqlite3.connect('app.sql')
+    connection.row_factory=sqlite3.Row
+    return connection
 
-# Fermeture de la connexion à la base de donnée
-def close_db(e=None):
-    db = g.pop('db', None)
-    if db is not None:
-        db.close()
-        
-# Activation de la base de donnée
-def init_db() :
-    # on ouvre l'acces à la bdd
-    db = get_db()
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-
-def init_app(app):
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
-
-@click.command('init-db')
-@with_appcontext
-def init_db_command():
-    """Nettoyer la base de donnée et recréer les tables"""
-    init_db()
-    click.echo('La base de donnée est initialisée')
-    
     
 
     
--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_caching import Cache
+import db
 
 app = Flask(__name__)
+cache = Cache(app)
 
 @app.route("/")
 def home() :
