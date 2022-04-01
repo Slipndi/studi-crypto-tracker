ed89c305309fc48a4c02f3980bf5ed3427b764c6
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-04-01 23:06:21 +0200

    mise à jour du commentaire

--- tasks_to_manage.py
+++ tasks_to_manage.py
@@ -1,6 +1,12 @@
 import app.main as main
 
 def scheduled_job():
+    """Tache plannifiée une fois par jour, 
+        Récupération des cryptomonnaies en base de données avec l'ensemble des détails
+        calcul de la rentabilité actuelle
+        insertion en base de donnée chaque action est découpée dans une fonctionnalité 
+        dans main.
+    """    
     cryptomonaies = main.get_crypto_from_database_with_details()
     amount = main.get_amount(cryptomonaies)
     main.insert_amount_in_database(amount)
