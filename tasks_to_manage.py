8d09acc71065c4edf03f494677175bf2ef32e612
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-04-01 09:03:18 +0200

    tasks to manage

---
+++ tasks_to_manage.py
@@ -0,0 +1,9 @@
+from app.main import get_crypto_from_database_with_details, get_amount
+from app.db import insert_amount_in_database
+
+def scheduled_job():
+    cryptomonaies = get_crypto_from_database_with_details()
+    amount = get_amount(cryptomonaies)
+    insert_amount_in_database(amount)
+    
+scheduled_job()
