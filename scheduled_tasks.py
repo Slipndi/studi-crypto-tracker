091f7f9004b056bd5b09e99b22c6c61f1d7b17bf
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-04-01 07:06:44 +0200

    Update horaire

--- scheduled_tasks.py
+++ scheduled_tasks.py
@@ -4,7 +4,7 @@ from app.db import insert_amount_in_database
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=5, minute=5)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=5, minute=15)
 def scheduled_job():
     cryptomonaies = get_crypto_from_database_with_details()
     amount = get_amount(cryptomonaies)
