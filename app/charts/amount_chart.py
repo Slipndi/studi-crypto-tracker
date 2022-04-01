cb239862ef9f98fb14acb12fde374056c9bc583a
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-04-01 08:56:12 +0200

    Ajout de commentaires

--- app/charts/amount_chart.py
+++ app/charts/amount_chart.py
@@ -1,11 +1,21 @@
 import base64
 from io import BytesIO
+
 from matplotlib.figure import Figure
+
 from ..db import get_amount
 
+# Définition de la couleur du graphique
 axis_color = 'white'
 
-def get_amount_chart() :
+def get_amount_chart() -> str :
+    """
+    Génération des datas en base64 du graphique d'investissement.
+    Je récupère les données de la base de donnée et je génère un .png en base64
+    afin de pouvoir ensuite l'envoyer à ma view
+    Returns:
+        string: base64 string image
+    """    
     fig = Figure(facecolor='white')
     ax = fig.subplots()
     data = get_amount()
@@ -14,8 +24,6 @@ def get_amount_chart() :
     ax.tick_params(color=axis_color, labelcolor=axis_color)
     ax.spines['bottom'].set_color(axis_color)
     ax.spines['left'].set_color(axis_color)
-    
-    ax.set_xlabel('date')
     ax.set_ylabel('valorisation')     
     ax.xaxis.label.set_color(axis_color)
     ax.yaxis.label.set_color(axis_color)
--- app/db/db.py
+++ app/db/db.py
@@ -3,20 +3,43 @@ from datetime import date
 from flask import flash
 
 def get_connection() :
+    """ Créer une connexion sqlite3 sur la bdd embarquée
+
+    Returns:
+        connection à app.sqlite
+    """    
     connection = sqlite3.connect('app.sqlite')
     connection.row_factory=sqlite3.Row
     return connection
 
-def get_crypto_in_database() :
+def get_crypto_in_database() -> list :
+    """Récupération de toutes les cryptomonnaies insérées dans la table crypto_value
+    groupées par leurs id, renvoyant le prix le plus cher et la somme totale d'unité en notre possession
+
+    Returns:
+        list: (int) crypto_id, (float) price, (int) quantity
+    """    
     connection=get_connection()
     cursor = connection.cursor()
     # récupération des id et prix dans la base de donnée
     cursor.execute("SELECT crypto_id, max(price) price, sum(quantity) quantity FROM crypto_value group by crypto_id order by crypto_id")
     data_from_local_database = cursor.fetchall()
     connection.close()
+    
     return data_from_local_database
 
-def insert_new_crypto_quantity(cryptomonnaie_id, cryptomonnaie_quantity, cryptomonnaie_name, cryptomonnaie_unique_price) :
+def insert_new_crypto_quantity(cryptomonnaie_id, cryptomonnaie_quantity, cryptomonnaie_name, cryptomonnaie_unique_price) -> None:
+    """ Insertion des valeurs dans la table crypto_value
+
+    Args:
+        cryptomonnaie_id (int): Identifiant unique de la cryptomonnaie fournit par l'api
+        cryptomonnaie_quantity (int): quantité renseignée
+        cryptomonnaie_name (str): nom de la cryptomonnaie fournit par l'api
+        cryptomonnaie_unique_price (float): valeur unitaire de la crypto lors de l'achat
+        
+    Returns:
+        None
+    """    
     connection = get_connection()
     cursor = connection.cursor()
     cursor.execute("INSERT INTO crypto_value (crypto_id, name, price, quantity, date) VALUES (?, ?, ?, ?, ?)",(cryptomonnaie_id, cryptomonnaie_name,cryptomonnaie_unique_price, cryptomonnaie_quantity, date.today()))
@@ -24,13 +47,30 @@ def insert_new_crypto_quantity(cryptomonnaie_id, cryptomonnaie_quantity, cryptom
     flash("Transaction Validée", "success")
     connection.close()
     
-def delete_crypto(cryptomonnaie_id) :
+def delete_crypto(cryptomonnaie_id) -> None :
+    """Suppression d'une cryptomonnaie dans la base de donnée sqlite
+
+    Args:
+        cryptomonnaie_id (int): Code unique de la cryptomonnaie fournit par l'api
+        
+    Returns : 
+        None
+    """    
     connection = get_connection()
     cursor = connection.cursor()
     cursor.execute('DELETE FROM crypto_value WHERE crypto_id = ?', (cryptomonnaie_id,))
     connection.commit()
 
-def update_crypto(cryptomonnaie_id, cryptomonnaie_quantity):
+def update_crypto(cryptomonnaie_id, cryptomonnaie_quantity) -> None:
+    """Mise à jour de la quantité de cryptomonnaie concerné 
+
+    Args:
+        cryptomonnaie_id (int): Identifiant unique de la cryptomonnaie fournit par l'api
+        cryptomonnaie_quantity (int): Nouvelle quantité renseignée
+    
+    Returns : 
+        None
+    """    
     connection = get_connection()
     cursor=connection.cursor()
     cursor.execute('SELECT id, crypto_id,quantity FROM crypto_value WHERE crypto_id = ? ORDER BY Quantity, price ', (cryptomonnaie_id,))
@@ -57,7 +97,15 @@ def update_crypto(cryptomonnaie_id, cryptomonnaie_quantity):
         flash("Mise à jour réussie", "success")
     connection.close()
     
-def insert_amount_in_database(amount):
+def insert_amount_in_database(amount) -> None :
+    """Insertion de la valorisation dans la table evolution_gain
+
+    Args:
+        amount (float): valorisation des cryptos présente en base 
+    
+    Returns :
+        None
+    """    
     connection = get_connection()
     cursor = connection.cursor()
     cursor.execute("INSERT INTO evolution_gain (value, date) VALUES (?, ?)",
@@ -67,6 +115,10 @@ def insert_amount_in_database(amount):
     connection.close()
     
 def get_amount() -> list :
+    """Retourne l'ensemble des valorisations de la table evolution_gain trié par date
+    Returns:
+        list: (date) date, (float) value 
+    """    
     connection = get_connection()
     cursor = connection.cursor()
     cursor.execute('SELECT date, value FROM evolution_gain ORDER BY Date')
