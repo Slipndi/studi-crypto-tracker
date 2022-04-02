b7c7e927b3b6e489670d3e91d2118c4e211df4e1
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-04-02 13:12:06 +0200

    mise à jour des tests et de remove.html

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,5 +1,23 @@
 from run import app
+import app.app as a
     
+
 def test_should_get_status_code_ok_on_index(client):
     response = client.get('/')
     assert response.status_code == 200
+
+def test_should_get_status_code_ok_on_remove(client):
+    response = client.get('/remove')
+    assert response.status_code == 200
+    
+def test_should_get_status_code_ok_on_add(client):
+    response = client.get('/add')
+    assert response.status_code == 200
+    
+def test_should_get_status_code_ok_on_amount_graph(client):
+    response = client.get('/amount-graph')
+    assert response.status_code == 200
+
+def test_should_get_all_amount_from_database_return_list():
+    result = a.get_all_amount_from_database()
+    assert isinstance(result, list)
