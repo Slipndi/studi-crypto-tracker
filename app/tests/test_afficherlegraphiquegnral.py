df880daa4cf53690cf180b00695b879343a26cbd
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-04-13 12:33:03 +0200

    Mise à jour des tests

--- app/tests/test_afficherlegraphiquegnral.py
+++ app/tests/test_afficherlegraphiquegnral.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestAfficherlegraphiquegnral():
