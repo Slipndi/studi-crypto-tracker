8248931f09dca786c96b1c282c02be662d33bd08
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-04-13 12:12:54 +0200

    update test

--- app/tests/test_suppressiondunecrypto.py
+++ app/tests/test_suppressiondunecrypto.py
@@ -2,6 +2,7 @@
 import pytest
 import time
 import json
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
